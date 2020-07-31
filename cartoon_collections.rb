c984562a0f1ad6364b78db4fb33c5fa4cb5e2898
Kyle Walters <kyle.p.walters@gmail.com>
2020-07-31 16:11:12 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -11,7 +11,7 @@ def summon_captain_planet(planeteer_calls)
 end
 
 def long_planeteer_calls(planeteer_calls)
-  planeteer_calls.each do |element|
+  planeteer_calls.any do |element|
     if element.length < 4
       return FALSE
   end
