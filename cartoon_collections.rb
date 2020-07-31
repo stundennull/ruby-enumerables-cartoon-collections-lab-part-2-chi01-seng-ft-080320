9c4becdbf36b3ec7bc669f463bdc5f0e42df9dba
Kyle Walters <kyle.p.walters@gmail.com>
2020-07-31 16:40:14 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -16,5 +16,5 @@ end
 
 def find_valid_calls(planeteer_calls)
   valid_calls = ["Earth!", "Wind!", "Fire!", "Water!", "Heart!"]
-  planeteer_calls.find { |element| element.include?(valid_calls) }
+  planeteer_calls.find { |element| valid_calls.include?(element) }
 end
